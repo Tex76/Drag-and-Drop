b56b41a84803431ef4a87ec91a5effe89d10076b
Tex <techcenter2001@gmail.com>
2023-12-18 10:42:12 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dragAndDrop',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
